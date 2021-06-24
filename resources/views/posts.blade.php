6e080280b316966f9572e686c781bb357ad011d7
AG <Aman Gupta>
2021-06-24 01:53:08 -0700

    Post Find and All class Working

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Blog</title>
+        <link rel="stylesheet" href="/app.css">
+    </head>
+
+    <body>
+        <?php foreach ($posts as $post) : ?>
+            <article>
+                <?= $post; ?>
+            </article>
+
+        <?php endforeach; ?>
+    </body>
+</html>
