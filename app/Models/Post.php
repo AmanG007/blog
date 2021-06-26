732f993d342684802157189cc108b53605861fdd
amang007 <aman40688@gmail.com>
2021-06-26 01:03:51 -0700

    cache done

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,24 +4,49 @@ namespace App\Models;
 
 use Illuminate\Support\Facades\File;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Cache\FileStore;
 
 class Post {
 
+    public $title;
+
+    public $excerpt;
+
+    public $body;
+
+    public $date;
+
+    public $slug;
+
+    public function __construct($title, $excerpt, $body, $date, $slug) {
+
+        $this -> title = $title;
+        $this -> excerpt = $excerpt;
+        $this -> body = $body;
+        $this -> date = $date;
+        $this -> slug = $slug;
+    }
+
     public static function all() {
 
-        $files = File::files(resource_path("posts/"));
-
-        return array_map(fn($file) => $file->getcontents(), $files);
+        return cache()->rememberForever('posts.all', function() {
+            return collect(File::files(resource_path("posts")))
+                ->map(fn($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn($document) => new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->body(),
+                    $document->date,
+                    $document->slug
+                ))
+                ->sortByDesc('date');
+        });
+        
     }
 
     public static function find($slug) {
-    
-        if (! file_exists($path = resource_path("posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
-
+        return static::all()->firstWhere('slug', $slug);
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
